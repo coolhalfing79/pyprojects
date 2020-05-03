a210c1669e640f6ef1dfb8189b144d3e7d16ed64
coolhalfing79 <anirudhachari27@gmail.com>
2020-05-03 18:58:45 +0530

    i dont know

--- heapSort.py
+++ heapSort.py
@@ -6,31 +6,31 @@ def swap(array, i1, i2):
 def heapify(array, start, end):
     left = 2 * start + 1
     right = 2 * start + 2
+    largest = start
     print(array, start)
-    if right <= end:
-        heapify(array, left, end)
+    if start < end:
         heapify(array, right, end)
-        if array[start] < array[left] or array[start] < array[right]:
-            if array[left] < array[right]:
-                swap(array, start, right)
-                heapify(array, right, end)
-            else:
-                swap(array, start, left)
-                heapify(array, left, end)
+        heapify(array, left, end)
+        if end >= left and array[largest] < array[left]:
+            largest = left
+        if end >= right and array[largest] < array[right]:
+            largest = right
+        if largest != start:
+            swap(array, start, largest)
+            heapify(array, largest, end)
     
 
 def heapSort(array):
     end = len(array) - 1
-    heapify(array, 0, end)
-    print()
     while end > 0:
+        heapify(array, 0, end)
         swap(array, 0, end)
         end -= 1
-        heapify(array, 0, end)
 
 def main():
     array = []
-    for x in range(10):
+    lim = 10
+    while len(array) < lim:
         array.append(randint(0, 100))
     heapSort(array)
     print(array)
